d7f0cbfbb4bac2d81031410e297fcf4c4fb5df65
code-sharad <codesharad@gmail.com>
2025-05-22 21:47:48 +0530

    add options..

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: process.env.VITE_FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONA"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
